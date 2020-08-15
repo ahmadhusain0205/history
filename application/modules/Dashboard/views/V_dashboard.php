373a71182589109e7f2cc9fd23775aff17d7a3b6
Ahmad Husain <ahmad.husain.ardiansyah11@gmail.com>
2020-08-15 14:47:33 +0700

    memperbaiki dashboard

--- application/modules/Dashboard/views/V_dashboard.php
+++ application/modules/Dashboard/views/V_dashboard.php
@@ -68,7 +68,7 @@
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Semesters</div>
+                                    <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Historis</div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">
                                         <?= $this->M_dashboard->get_data('semester')->num_rows(); ?>
                                     </div>
