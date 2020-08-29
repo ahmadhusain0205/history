4439638f9f82608b684374da6d096fafd615354f
Ahmad Husain <ahmad.husain.ardiansyah11@gmail.com>
2020-08-29 13:50:36 +0700

    add database

--- database/history.sql
+++ database/history.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Agu 2020 pada 06.44
+-- Waktu pembuatan: 29 Agu 2020 pada 08.49
 -- Versi server: 10.1.36-MariaDB
 -- Versi PHP: 7.2.11
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `umm`
+-- Database: `history`
 --
 
 -- --------------------------------------------------------
@@ -49,6 +49,7 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 
 CREATE TABLE `course` (
   `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `course` varchar(200) NOT NULL,
   `sks` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -61,7 +62,8 @@ CREATE TABLE `course` (
 
 CREATE TABLE `score` (
   `id` int(11) NOT NULL,
-  `grade` char(2) NOT NULL
+  `id_user` int(11) NOT NULL,
+  `grade` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -74,6 +76,7 @@ CREATE TABLE `semester` (
   `id` int(11) NOT NULL,
   `id_course` int(11) NOT NULL,
   `id_score` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `semester` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -103,21 +106,24 @@ ALTER TABLE `admin`
 -- Indeks untuk tabel `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indeks untuk tabel `score`
 --
 ALTER TABLE `score`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indeks untuk tabel `semester`
 --
 ALTER TABLE `semester`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_score` (`id_score`),
-  ADD KEY `id_course` (`id_course`);
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_course` (`id_course`),
+  ADD KEY `id_score` (`id_score`);
 
 --
 -- Indeks untuk tabel `user`
@@ -133,42 +139,55 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `course`
 --
 ALTER TABLE `course`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT untuk tabel `score`
 --
 ALTER TABLE `score`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `semester`
 --
 ALTER TABLE `semester`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
+-- Ketidakleluasaan untuk tabel `course`
+--
+ALTER TABLE `course`
+  ADD CONSTRAINT `course_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `score`
+--
+ALTER TABLE `score`
+  ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
 -- Ketidakleluasaan untuk tabel `semester`
 --
 ALTER TABLE `semester`
-  ADD CONSTRAINT `semester_ibfk_1` FOREIGN KEY (`id_score`) REFERENCES `score` (`id`),
-  ADD CONSTRAINT `semester_ibfk_2` FOREIGN KEY (`id_course`) REFERENCES `course` (`id`);
+  ADD CONSTRAINT `semester_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `semester_ibfk_2` FOREIGN KEY (`id_course`) REFERENCES `course` (`id`),
+  ADD CONSTRAINT `semester_ibfk_3` FOREIGN KEY (`id_score`) REFERENCES `score` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
